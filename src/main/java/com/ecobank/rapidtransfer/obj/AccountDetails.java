d11947c94da3507f598abb05074321bff871f031
Emmanuella Bamgbose <emmanuellabamgbose@Emmanuellas-MacBook-Pro.local>
2019-09-25 14:12:05 +0100

    new accountDetail class

---
+++ src/main/java/com/ecobank/rapidtransfer/obj/AccountDetails.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ecobank.rapidtransfer.obj;
+
+
+/**
+ *
+ * @author emmanuellabamgbose
+ */
+public class AccountDetails {
+     private int accountNo;
+     private String accountName;
+}
